18f95110e8bb10738e6a16efa973025a7f7f1fe3
Diego França <diego.lightbase@sebraemg.com.br>
2025-12-24 10:52:45 -0300

    test: create test to make validation when return not allowed external service

--- tests/Unit/TransferMoneyTest.php
+++ tests/Unit/TransferMoneyTest.php
@@ -118,4 +118,37 @@ class TransferMoneyTest extends TestCase
         );
 
     }
+    public function test_should_return_exception_when_external_service_return_not_allowed()
+    {
+        // GIVEN
+        $payer = new User(
+            uuid: new Password(4),
+            fullname: 'Diego franca',
+            document: new Document('07634403694'),
+            email: new Email('diego.tg.franca@gmail.com'),
+            wallet: new Wallet(amount: new Amount(40000)),
+            type: UserType::REGULAR
+        );
+
+
+        $payee = new User(
+            uuid: new Password(4),
+            fullname: 'Diego franca',
+            document: new Document('07634403694'),
+            email: new Email('diego.tg.franca@gmail.com'),
+            wallet: new Wallet(amount: new Amount(5000)),
+            type: UserType::REGULAR
+        );
+        $useCase = new TranferMoney();
+
+        // WHEN
+        $this->expectException(TransferNotAllowedException::class);
+        $this->expectExceptionMessage('Transfer not allowed.');
+        $useCase->execute(
+            payer: $payer,
+            payee: $payee,
+            amount: new Amount(10000)
+        );
+
+    }
 }
